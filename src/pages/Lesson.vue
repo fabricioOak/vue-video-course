61320692a4c2e0d2e2a2646529f43b042c611abe
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-20 00:44:36 -0300

    add Lesson page

---
+++ src/pages/Lesson.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lesson-page">
+    <header>
+      <p>
+        <router-link :to="`/courses/${course.id}`"
+          >Back to {{ course.title }}</router-link
+        >
+      </p>
+      <h1>{{ lesson.title }}</h1>
+    </header>
+    <div class="content">
+      <vue-vimeo-player
+        :video-id="lesson.videoId"
+        :options="{ responsive: true }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import courses from "../courses";
+import { useRoute } from "vue-router";
+import { vueVimeoPlayer } from "vue-vimeo-player";
+
+const route = useRoute();
+const { courseId, lessonId } = route.params;
+const course = courses.find((course) => course.id === parseInt(courseId));
+const lesson = course.lessons.find(
+  (lesson) => lesson.id === parseInt(lessonId)
+);
+</script>
