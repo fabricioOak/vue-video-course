16a01c3c483811e21e97e57642b394c9c49cc9eb
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-20 00:11:37 -0300

    new CourseSummary component

---
+++ src/components/CourseSummary.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="summary">
+    <div>
+      <div class="title">
+        <h2>
+          <router-link :to="{name: 'Course', params : {courseId: course.id}}">
+            {{course.title}}
+          </router-link>
+        </h2>
+      </div>
+      <p>
+        <router-link :to="{name: 'Course', params : {courseId: course.id}}">
+          {{course.description}}
+        </router-link>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  defineProps({
+    course: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
+
+<style>
+
+</style>
