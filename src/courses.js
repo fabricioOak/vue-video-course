e6a39a82a34346a8e87a02743955bda4a8cb66ad
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-19 23:43:20 -0300

    add mockup data for courses

---
+++ src/courses.js
@@ -0,0 +1,42 @@
+const courses = [
+  {
+    id: 1,
+    title: 'Linux for Beginners',
+    description: 'Learn Linux from scratch',
+    lessons: [
+      {
+        id: 1,
+        title: 'Introduction',
+        description: 'Here you will learn how to install Linux on your computer.',
+        videoId: 6546540,
+      },
+      {
+        id: 2,
+        title: 'First Steps',
+        description: 'In this lesson you will see the basic commands of Linux.',
+        videoId: 6546540,
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Vue.js for Beginners',
+    description: 'Learn Vue.js from scratch',
+    lessons: [
+      {
+        id: 1,
+        title: 'Introduction',
+        description: 'Here you will learn how to install Vue.js on your computer.',
+        videoId: 6546540,
+      },
+      {
+        id: 2,
+        title: 'Hello World in Vue.js',
+        description: 'In this lesson you will see the basic of Vue.js.',
+        videoId: 6546540,
+      }
+    ]
+  }
+]
+
+export default courses
