49d1be7f6c2a7b53660381319459d94af4f01044
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-20 00:12:02 -0300

    update Home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home-page">
+    <header>
+      <h1>Online Course Site</h1>
+    </header>
+    <CourseSummary v-for="course in courses" :key="course.id" :course="course"/>
+  </div>
+</template>
+
+<script setup>
+import courses from '../courses';
+import CourseSummary from '../components/CourseSummary.vue';
+</script>
