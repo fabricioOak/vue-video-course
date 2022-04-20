125669564fa2eb903c014a2112895fb786acea64
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-20 00:31:19 -0300

    Course page and LessonSummary component add

---
+++ src/components/LessonSummary.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>
+      <div class="title">
+        <h2>
+          <router-link :to="`/courses/${courseId}/lessons/${lesson.id}`">
+            {{ num }}. {{ lesson.title }}
+          </router-link>
+        </h2>
+      </div>
+      <p>
+        <router-link :to="`/courses/${courseId}/lessons/${lesson.id}`">
+          {{ lesson.description }}
+        </router-link>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  courseId: {
+    type: String,
+    required: true,
+  },
+  num: {
+    type: Number,
+    required: true,
+  },
+  lesson: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
---
+++ src/pages/Course.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="course-page">
+    <header>
+      <p>
+        <router-link :to="{ name: 'Home' }">Back to courses</router-link>
+      </p>
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <router-link :to="`/courses/${courseId}/lessons/${course.lessons[0].id}`">
+        Start Course
+      </router-link>
+    </header>
+  </div>
+  <div>
+    <LessonSummary
+      v-for="(lesson, index) in lessons"
+      :key="index"
+      :course-id="courseId"
+      :lesson="lesson"
+      :num="index + 1"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import courses from "../courses";
+import LessonSummary from "../components/LessonSummary.vue";
+
+const route = useRoute();
+const courseId = route.params.courseId;
+
+const course = courses.find((course) => course.id === parseInt(courseId));
+const { title, lessons } = course;
+</script>
