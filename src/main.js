d12b2c94a2929f2f726091f3fdd70dfbed06acd2
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-04-21 00:33:37 -0300

    bootstrap fix

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
-import './assets/bootstrap/bootstrap.min'
-import './assets/bootstrap/bootstrap.min.css'
 
 const app = createApp(App)
 app.use(router)
